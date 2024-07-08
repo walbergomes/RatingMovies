5229ec13743a67d94df46362d9512f3b3f2ec910
Walber <walberg.s48@gmail.com>
2024-07-08 18:53:39 -0300

    :sparkles: add DiskStorage class

---
+++ api/src/providers/DiskStorage.js
@@ -0,0 +1,28 @@
+const fs = require("fs") // lida com manipulação de arquivos
+const path = require("path")
+const uploadConfig = require("../config/upload")
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    )
+
+    return file
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+
+    try {
+      await fs.promises.stat(filePath)
+    } catch (error) {
+      return
+    }
+
+    await fs.promises.unlink(filePath)
+  }
+}
+
+module.exports = DiskStorage
