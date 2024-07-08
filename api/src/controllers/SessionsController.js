68458f1023f8b623630004cd2e77185169954197
Walber <walberg.s48@gmail.com>
2024-07-07 21:51:28 -0300

    :safety_vest: add user register validation

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,8 +1,17 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
 class SessionsController {
-  create(req, res) {
+  async create(req, res) {
     const { email, password } = req.body
 
-    return res.json({ email, password })
+    const user = await knex("users").where({ email }).first()
+
+    if(!user) {
+      throw new AppError("Email e/ou senha incorreta", 401)
+    }
+
+    return res.json(user)
   }
 }
 
