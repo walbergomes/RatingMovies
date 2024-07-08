2621a909b4130b34391672c4172149d92508b010
Walber <walberg.s48@gmail.com>
2024-07-08 19:16:54 -0300

    :sparkles: loading imagens

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require("express")
 const usersRoutes = Router()
 
+const multer = require("multer")
+const uploadConfig = require("../config/upload")
+const upload = multer(uploadConfig.MULTER)
+
 const UsersController = require("../controllers/UsersController")
 const usersController = new UsersController()
 
@@ -8,5 +12,9 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename)
+  res.json()
+})
 
 module.exports = usersRoutes
