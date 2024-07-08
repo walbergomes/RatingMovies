6828ef32148659ca45c63540cdfc7ad5dc92b35a
Walber <walberg.s48@gmail.com>
2024-07-07 21:56:13 -0300

    :safety_vest: password validation

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
 
 class SessionsController {
   async create(req, res) {
@@ -8,6 +9,11 @@ class SessionsController {
     const user = await knex("users").where({ email }).first()
 
     if(!user) {
+      throw new AppError("Email e/ou senha incorreta", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+    if(!passwordMatched) {
       throw new AppError("Email e/ou senha incorreta", 401)
     }
 
