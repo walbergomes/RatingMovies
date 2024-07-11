defde2a91fec2b46b0c3c25c5b5ad27e56bb36ba
Walber <walberg.s48@gmail.com>
2024-07-11 19:24:52 -0300

    :sparkles: using localStorage

--- front/src/hooks/auth.jsx
+++ front/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api"
 
@@ -13,11 +13,13 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password })
       const { user, token } = response.data
 
+      localStorage.setItem("@ratingmovies:user", JSON.stringify(user))
+      localStorage.setItem("@ratingmovies:token", token)
+
       api.defaults.headers.common["Authorization"] = `Baerer ${token}`
       setData({ user, token })
 
       // console.log({user, token})
-
     } catch (error) {
       if(error.response) {
         alert(error.response.data.message)
@@ -25,8 +27,21 @@ function AuthProvider({ children }) {
         alert("Não foi pissível entrar")
       }
     }
-    
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@ratingmovies:token")
+    const user = localStorage.getItem("@ratingmovies:user")
+
+    if(token && user) {
+      api.defaults.headers.common["Authorization"] = `Baerer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
 
   return (
     <authContext.Provider value={{ signIn, user: data.user }} >
