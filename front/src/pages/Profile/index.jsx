64ece1a935da2f56e86574ffe08cc6ca7361d5cb
Walber <walberg.s48@gmail.com>
2024-07-11 19:37:51 -0300

    :art: loading data user in the profile page

--- front/src/pages/Profile/index.jsx
+++ front/src/pages/Profile/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Form, Avatar } from "./styles";
 import { Link } from "react-router-dom";
 
@@ -6,7 +7,16 @@ import { MdOutlineEmail, MdOutlineLock, MdOutlinePersonOutline, MdOutlineArrowBa
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
+import { useAuth } from "../../hooks/auth"
+
 export function Profile() {
+  const { user } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
   return (
     <Container>
       <header>
@@ -35,24 +45,30 @@ export function Profile() {
             placeholder="Nome"
             type="text"
             icon={MdOutlinePersonOutline}
+            value={name}
+            onChange={e => setName(e.target.value)}
           />
 
           <Input 
             placeholder="Email"
             type="email"
             icon={MdOutlineEmail}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
 
           <Input 
             placeholder="Senha atual"
             type="password"
             icon={MdOutlineLock}
+            onChange={e => setPasswordOld(e.target.value)}
           />
 
           <Input 
             placeholder="Nova senha"
             type="password"
             icon={MdOutlineLock}
+            onChange={e => setPasswordNew(e.target.value)}
           />
 
           <Button title="Salvar" />
