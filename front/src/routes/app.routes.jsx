9b4e4e5227e1ac1d5597052df92fb998864e3313
Walber <walberg.s48@gmail.com>
2024-07-15 19:01:59 -0300

    :bug: tratando rotaas indisponiveis

--- front/src/routes/app.routes.jsx
+++ front/src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 import { Home } from "../pages/Home"
 import { New } from "../pages/New"
@@ -12,6 +12,8 @@ export function AppRoutes() {
       <Route path="/new" element={<New />}/>
       <Route path="/profile" element={<Profile />}/>
       <Route path="/preview/:id" element={<Preview />}/>
+
+      <Route path="*" element={<Navigate to="/"/>} />
     </Routes>
   )
 }
